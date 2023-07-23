5d381c1708b5b38260ba078203e68b9af37fdcb9
Ahmed Saied <ahmed1337saied@gmail.com>
2023-07-23 16:50:12 +0300

    Init commit

---
+++ mage-files/transform.py
@@ -0,0 +1,142 @@
+import pandas as pd 
+if 'transformer' not in globals():
+    from mage_ai.data_preparation.decorators import transformer
+if 'test' not in globals():
+    from mage_ai.data_preparation.decorators import test
+
+def prepare_datetime_dim(df):
+
+    #extract original columns
+    datetime_dim = df[['tpep_pickup_datetime','tpep_dropoff_datetime']].drop_duplicates().reset_index(drop=True)
+    
+    #pick time
+    datetime_dim['pick_hour'] = datetime_dim['tpep_pickup_datetime'].dt.hour
+    datetime_dim['pick_day'] = datetime_dim['tpep_pickup_datetime'].dt.day
+    datetime_dim['pick_month'] = datetime_dim['tpep_pickup_datetime'].dt.month
+    datetime_dim['pick_year'] = datetime_dim['tpep_pickup_datetime'].dt.year
+    datetime_dim['pick_weekday'] = datetime_dim['tpep_pickup_datetime'].dt.weekday
+
+    #drop time
+    datetime_dim['drop_hour'] = datetime_dim['tpep_dropoff_datetime'].dt.hour
+    datetime_dim['drop_day'] = datetime_dim['tpep_dropoff_datetime'].dt.day
+    datetime_dim['drop_month'] = datetime_dim['tpep_dropoff_datetime'].dt.month
+    datetime_dim['drop_year'] = datetime_dim['tpep_dropoff_datetime'].dt.year
+    datetime_dim['drop_weekday'] = datetime_dim['tpep_dropoff_datetime'].dt.weekday
+
+    #set index
+    datetime_dim['datetime_id'] = datetime_dim.index
+
+    return datetime_dim[['datetime_id', 'tpep_pickup_datetime', 'pick_hour', 'pick_day', 'pick_month', 'pick_year', 'pick_weekday',
+                             'tpep_dropoff_datetime', 'drop_hour', 'drop_day', 'drop_month', 'drop_year', 'drop_weekday']]
+   
+def prepare_passenger_count_dim(df):
+    passenger_count_dim = df[['passenger_count']].drop_duplicates().reset_index(drop=True)
+    passenger_count_dim['passenger_count_id'] = passenger_count_dim.index
+    return passenger_count_dim[['passenger_count_id','passenger_count']]
+
+def prepare_trip_distance_dim(df):
+    trip_distance_dim = df[['trip_distance']].drop_duplicates().reset_index(drop=True)
+    trip_distance_dim['trip_distance_id'] = trip_distance_dim.index
+    return trip_distance_dim[['trip_distance_id','trip_distance']]
+
+def prepare_rate_code_dim(df):
+    rate_code_type = {
+        1:"Standard rate",
+        2:"JFK",
+        3:"Newark",
+        4:"Nassau or Westchester",
+        5:"Negotiated fare",
+        6:"Group ride"
+    }
+
+    rate_code_dim = df[['RatecodeID']].drop_duplicates().reset_index(drop=True)
+    rate_code_dim['rate_code_id'] = rate_code_dim.index
+    rate_code_dim['rate_code_name'] = rate_code_dim['RatecodeID'].map(rate_code_type)
+    return rate_code_dim[['rate_code_id','RatecodeID','rate_code_name']]
+
+def prepare_pickup_location_dim(df):
+    pickup_location_dim = df[['pickup_longitude', 'pickup_latitude']].drop_duplicates().reset_index(drop=True)
+    pickup_location_dim['pickup_location_id'] = pickup_location_dim.index
+    return pickup_location_dim[['pickup_location_id','pickup_latitude','pickup_longitude']] 
+
+def prepare_dropoff_location_dim(df):
+    dropoff_location_dim = df[['dropoff_longitude', 'dropoff_latitude']].drop_duplicates().reset_index(drop=True)
+    dropoff_location_dim['dropoff_location_id'] = dropoff_location_dim.index
+    return dropoff_location_dim[['dropoff_location_id','dropoff_latitude','dropoff_longitude']]
+
+def prepare_payment_type_dim(df):
+    payment_type_name = {
+        1:"Credit card",
+        2:"Cash",
+        3:"No charge",
+        4:"Dispute",
+        5:"Unknown",
+        6:"Voided trip"
+    }
+    payment_type_dim = df[['payment_type']].drop_duplicates().reset_index(drop=True)
+    payment_type_dim['payment_type_id'] = payment_type_dim.index
+    payment_type_dim['payment_type_name'] = payment_type_dim['payment_type'].map(payment_type_name)
+    return payment_type_dim[['payment_type_id','payment_type','payment_type_name']]
+
+@transformer
+def transform(df, *args, **kwargs):
+    """
+    Template code for a transformer block.
+
+    Add more parameters to this function if this block has multiple parent blocks.
+    There should be one parameter for each output variable from each parent block.
+
+    Args:
+        data: The output from the upstream parent block
+        args: The output from any additional upstream blocks (if applicable)
+
+    Returns:
+        Anything (e.g. data frame, dictionary, array, int, str, etc.)
+    """
+    df['tpep_pickup_datetime'] = pd.to_datetime(df['tpep_pickup_datetime'])
+    df['tpep_dropoff_datetime'] = pd.to_datetime(df['tpep_dropoff_datetime'])
+    
+    datetime_dim = prepare_datetime_dim(df)
+
+    passenger_count_dim = prepare_passenger_count_dim(df)
+
+    trip_distance_dim = prepare_trip_distance_dim(df)
+    
+    rate_code_dim = prepare_rate_code_dim(df)
+
+    pickup_location_dim = prepare_pickup_location_dim(df)
+
+    dropoff_location_dim = prepare_dropoff_location_dim(df)
+
+    payment_type_dim = prepare_payment_type_dim(df)
+
+    trip_fact = (
+        df.merge(passenger_count_dim, on='passenger_count') 
+        .merge(trip_distance_dim, on='trip_distance') 
+        .merge(rate_code_dim, on='RatecodeID') 
+        .merge(pickup_location_dim, on=['pickup_longitude', 'pickup_latitude']) \
+        .merge(dropoff_location_dim, on=['dropoff_longitude', 'dropoff_latitude'])\
+        .merge(datetime_dim, on=['tpep_pickup_datetime','tpep_dropoff_datetime']) \
+        .merge(payment_type_dim, on='payment_type') 
+        [['VendorID', 'datetime_id', 'passenger_count_id',
+        'trip_distance_id', 'rate_code_id', 'store_and_fwd_flag', 'pickup_location_id', 'dropoff_location_id',
+        'payment_type_id', 'fare_amount', 'extra', 'mta_tax', 'tip_amount', 'tolls_amount',
+        'improvement_surcharge', 'total_amount']]
+               )
+    
+    return {"datetime_dim":datetime_dim.to_dict(orient="dict"),
+            "passenger_count_dim":passenger_count_dim.to_dict(orient="dict"),
+            "trip_distance_dim":trip_distance_dim.to_dict(orient="dict"),
+            "rate_code_dim":rate_code_dim.to_dict(orient="dict"),
+            "pickup_location_dim":pickup_location_dim.to_dict(orient="dict"),
+            "dropoff_location_dim":dropoff_location_dim.to_dict(orient="dict"),
+            "payment_type_dim":payment_type_dim.to_dict(orient="dict"),
+            "trip_fact":trip_fact.to_dict(orient="dict")}
+
+
+@test
+def test_output(output, *args) -> None:
+    """
+    Template code for testing the output of the block.
+    """
+    assert output is not None, 'The output is undefined'
